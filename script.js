4d1d0adb57147dcdaed28623bf1afe0096a1dcda
Milanna <brookins.mib@gmail.com>
2024-04-16 20:25:41 -0400

    Gallery test

--- script.js
+++ script.js
@@ -2,3 +2,32 @@
 window.addEventListener('contextmenu', function (e) {
     e.preventDefault();
 });
+
+// Makes gallery functional
+let slideIndex = 0;
+
+function showSlides() {
+    const slides = document.querySelectorAll('.slides img');
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    }
+    if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slides[slideIndex].style.display = 'block';
+}
+
+function prevSlide() {
+    slideIndex--;
+    showSlides();
+}
+
+function nextSlide() {
+    slideIndex++;
+    showSlides();
+}
+
+showSlides();
