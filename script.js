6d902815378089a09496651b6ce9bd38cbea5d5b
Milanna <brookins.mib@gmail.com>
2024-04-21 10:53:44 -0400

    Added smooth scrolling behavior for strand-list

--- script.js
+++ script.js
@@ -46,3 +46,14 @@ function nextSlide() {
 }
 
 showSlides();
+
+// Smooth scrolling behavior ... test later
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
