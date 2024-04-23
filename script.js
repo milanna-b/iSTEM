c06d8d2664efde7170f61af073f4a54fa8a10247
milanna-b <95477997+milanna-b@users.noreply.github.com>
2024-04-23 08:54:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function enableDarkMode() {
     document.body.classList.add('dark-mode');
 
     // Add dark-mode class to other necessary elements
-    document.querySelectorAll('.nav, h1, h2, .mode-switch, .mode-switch-label, footer, .footer-bottom, .istemp-info-button, .form-container, .form-button').forEach(function(element) {
+    document.querySelectorAll('.nav, h1, h2, footer, .footer-bottom, .istemp-info-button, .form-container, .form-button').forEach(function(element) {
         element.classList.add('dark-mode');
     });
 
